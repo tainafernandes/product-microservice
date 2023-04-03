8f337ebf058da7dabd661b1be55653b6294f80fa
tainafernandes <tainafefe@gmail.com>
2023-04-03 10:36:22 -0300

    Adding modelmapper dependecie

--- product-api/src/main/java/io/github/tainafernandes/productapi/DTO/Response/ProductDTOResponse.java
+++ product-api/src/main/java/io/github/tainafernandes/productapi/DTO/Response/ProductDTOResponse.java
@@ -4,9 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Getter
